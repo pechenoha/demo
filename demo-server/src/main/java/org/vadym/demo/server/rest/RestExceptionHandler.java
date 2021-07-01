a89139c448f9808ba46929a4a76ba452f7986afe
Vadym Pechenoha <vadym.pechenoha-ext@censhare.com>
2021-07-01 16:19:13 +0300

    feat: introduce CannotEditException

--- demo-server/src/main/java/org/vadym/demo/server/rest/RestExceptionHandler.java
+++ demo-server/src/main/java/org/vadym/demo/server/rest/RestExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.vadym.demo.server.model.exception.CannotEditException;
 import org.vadym.demo.server.model.exception.ExceptionResponseEntity;
 import org.vadym.demo.server.model.exception.NoSuchDocumentException;
 
@@ -19,5 +20,10 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(NoSuchDocumentException.class)
     protected ResponseEntity<ExceptionResponseEntity> handleException(NoSuchDocumentException e) {
         return new ResponseEntity<>(ExceptionResponseEntity.from(e), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(CannotEditException.class)
+    protected ResponseEntity<ExceptionResponseEntity> handleException(CannotEditException e) {
+        return new ResponseEntity<>(ExceptionResponseEntity.from(e), HttpStatus.FORBIDDEN);
     }
 }
--- demo-server/src/main/java/org/vadym/demo/server/storage/InMemoryStorage.java
+++ demo-server/src/main/java/org/vadym/demo/server/storage/InMemoryStorage.java
@@ -1,9 +1,11 @@
 package org.vadym.demo.server.storage;
 
 import org.springframework.stereotype.Component;
+import org.vadym.demo.server.model.exception.CannotEditException;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 /**
@@ -18,6 +20,11 @@ public class InMemoryStorage implements SearchEngineStorage {
 
     @Override
     public void put(String documentKey, String documentContents) {
+        // from the requirements: there will be no overwrites of a key with a new document
+        if (Objects.nonNull(documents.get(documentKey))) {
+            throw new CannotEditException();
+        }
+
         documents.put(documentKey, documentContents);
     }
 

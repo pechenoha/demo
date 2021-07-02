492adcd185cf9e8ea441d97906b524d39c767160
Vadym Pechenoha <vadym.pechenoha-ext@censhare.com>
2021-07-02 15:55:11 +0300

    fix: set the right package for client

---
+++ demo-client/src/main/java/org/vadym/demo/client/rest/RestExceptionHandler.java
@@ -0,0 +1,28 @@
+package org.vadym.demo.client.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.vadym.demo.client.exception.ExceptionResponseEntity;
+import org.vadym.demo.client.exception.SimpleSearchEngineException;
+
+/**
+ * Global exception handler for the Client application.
+ *
+ * @author Vadym Pechenoha
+ */
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(RestExceptionHandler.class);
+
+    @ExceptionHandler(SimpleSearchEngineException.class)
+    protected ResponseEntity<ExceptionResponseEntity> handleException(SimpleSearchEngineException e) {
+        logger.warn(e.getMessage());
+        return new ResponseEntity<>(ExceptionResponseEntity.from(e), HttpStatus.BAD_REQUEST);
+    }
+}
